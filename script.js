548e4c523113914fe11d696c06f3c5bcba7547f2
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-11 21:01:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let time = document.getElementById("timer");
+
+let currDate = new Date();
+
+setInterval(
+	function () {
+		currDate = new Date();
+
+		time.innerHTML = currDate.toLocaleString();
+	},1000
+)
